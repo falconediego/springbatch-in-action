2acf9f8288588e74792dbd194bfe5182357678b6
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2010-04-26 16:07:49 +0000

    ch. 01: polishing + chunk sample.

---
+++ sbia/ch01/src/test/resources/com/manning/sbia/ch01/chunk/insert-data.sql
@@ -0,0 +1,9 @@
+insert into invoice (id,customer_id,description,issue_date,amount) values('PR....214',9737,'','2010-11-18',102.23);
+insert into invoice (id,customer_id,description,issue_date,amount) values('PR....215',9737,'','2010-11-18',102.23);
+insert into invoice (id,customer_id,description,issue_date,amount) values('PR....216',9737,'','2010-11-18',102.23);
+insert into invoice (id,customer_id,description,issue_date,amount) values('PR....217',9737,'','2010-11-18',102.23);
+insert into invoice (id,customer_id,description,issue_date,amount) values('PR....218',9737,'','2010-11-18',102.23);
+insert into invoice (id,customer_id,description,issue_date,amount) values('PR....219',9737,'','2010-11-18',102.23);
+insert into invoice (id,customer_id,description,issue_date,amount) values('PR....220',9737,'','2010-11-18',102.23);
+insert into invoice (id,customer_id,description,issue_date,amount) values('PR....221',9737,'','2010-11-18',102.23);
+insert into invoice (id,customer_id,description,issue_date,amount) values('PR....222',9737,'','2010-11-18',102.23);
