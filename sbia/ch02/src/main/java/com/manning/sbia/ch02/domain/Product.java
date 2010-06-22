e26307bf155e3ed7da88d1a898bd302e71668252
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2010-06-22 13:27:49 +0000

    ch. 02: added product sample, invoice sample is deprecated.

---
+++ sbia/ch02/src/main/java/com/manning/sbia/ch02/domain/Product.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package com.manning.sbia.ch02.domain;
+
+import java.math.BigDecimal;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+public class Product {
+
+	public Product(String id) {
+		super();
+		this.id = id;
+	}
+	
+	public Product() {
+	}
+
+	private String id;
+	
+	private String name;
+	
+	private String description;
+	
+	private BigDecimal price;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+}
