48c9be6e39cfafd6d629c896abccca6a0ee52cb4
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2010-07-23 15:11:07 +0000

    ch. 11: added HTTP job launching sample, improved launching test with a pollable channel for assertions.

--- trunk/sbia/ch11/src/test/java/com/manning/sbia/ch11/integration/JobLaunchingMessageHandlerTest.java
+++ trunk/sbia/ch11/src/test/java/com/manning/sbia/ch11/integration/JobLaunchingMessageHandlerTest.java
@@ -3,34 +3,24 @@
  */
 package com.manning.sbia.ch11.integration;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
 import java.sql.Timestamp;
 import java.util.Collections;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.xml.transform.Result;
-import javax.xml.transform.stream.StreamResult;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.integration.channel.ChannelResolver;
+import org.springframework.integration.channel.PollableChannel;
 import org.springframework.integration.core.MessageChannel;
 import org.springframework.integration.message.MessageBuilder;
-import org.springframework.oxm.Marshaller;
-import org.springframework.oxm.XmlMappingException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -53,11 +43,16 @@ public class JobLaunchingMessageHandlerTest {
 	@Autowired
 	private SimpleTasklet tasklet;
 	
+	@Autowired
+	@Qualifier("job-executions")
+	private PollableChannel jobExecutionsChannel;
+	
 	@Before public void setUp() {
 		tasklet.clear();
 	}
 	
 	@Test public void launch() {
+		Assert.assertNull(jobExecutionsChannel.receive(1));
 		Timestamp currentDate = new Timestamp(System.currentTimeMillis());
 		jobRequestsChannel.send(
 			MessageBuilder.withPayload(
@@ -65,15 +60,18 @@ public class JobLaunchingMessageHandlerTest {
 				.build()
 		);		
 		Assert.assertEquals(currentDate.toString(),tasklet.getParameters().get("date"));
+		Assert.assertNotNull(jobExecutionsChannel.receive(1));
 	}
 	
 	@Test public void launchFromString() {
+		Assert.assertNull(jobExecutionsChannel.receive(1));
 		Timestamp currentDate = new Timestamp(System.currentTimeMillis());
 		jobRequestsAsStringChannel.send(
 			MessageBuilder.withPayload("simpleJob[date="+currentDate.toString()+"]")
 				.build()
 		);		
-		Assert.assertEquals(currentDate.toString(),tasklet.getParameters().get("date"));		
+		Assert.assertEquals(currentDate.toString(),tasklet.getParameters().get("date"));	
+		Assert.assertNotNull(jobExecutionsChannel.receive(1));
 	}
 	
 	public static class SimpleTasklet implements Tasklet {
@@ -84,7 +82,6 @@ public class JobLaunchingMessageHandlerTest {
 		public RepeatStatus execute(StepContribution contribution,
 				ChunkContext chunkContext) throws Exception {
 			parameters = new HashMap<String, Object>(chunkContext.getStepContext().getJobParameters());
-			Thread.sleep(1000);
 			return RepeatStatus.FINISHED;
 		}
 		
