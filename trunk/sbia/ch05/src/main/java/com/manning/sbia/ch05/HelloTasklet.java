c9815c1fce8c120fbce4f53a0266888e5eb34172
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2010-08-23 07:02:28 +0000

    ch. 05: unit tests on DefaultJobParametersConverter.

--- trunk/sbia/ch05/src/main/java/com/manning/sbia/ch05/HelloTasklet.java
+++ trunk/sbia/ch05/src/main/java/com/manning/sbia/ch05/HelloTasklet.java
@@ -3,6 +3,10 @@
  */
 package com.manning.sbia.ch05;
 
+import java.util.Map;
+
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -20,7 +24,18 @@ public class HelloTasklet implements Tasklet {
 	@Override
 	public RepeatStatus execute(StepContribution contribution,
 			ChunkContext chunkContext) throws Exception {
-		System.out.println("Hello world!");
+		JobParameters jobParameters = chunkContext.getStepContext().getStepExecution().getJobParameters();
+		if(jobParameters.isEmpty()) {
+			System.out.println("No job parameters!");
+		} else {
+			System.out.println("Job parameters:");
+			for(Map.Entry<String, JobParameter> param : jobParameters.getParameters().entrySet()) {
+				System.out.println(String.format(
+					"%s = %s (%s)",
+					param.getKey(),param.getValue().getValue(),param.getValue().getType()
+				));
+			}
+		}
 		return RepeatStatus.FINISHED;
 	}
 
