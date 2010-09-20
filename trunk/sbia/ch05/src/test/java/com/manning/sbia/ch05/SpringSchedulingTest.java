bc4f71cc5e2cc0d0eeda3de1bf689bbc9bd45be1
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2010-09-20 09:03:59 +0000

    ch. 05: added web samples (embedding in web app and simple web controller to launch batch).

--- trunk/sbia/ch05/src/test/java/com/manning/sbia/ch05/SpringSchedulingTest.java
+++ trunk/sbia/ch05/src/test/java/com/manning/sbia/ch05/SpringSchedulingTest.java
@@ -9,10 +9,6 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -37,22 +33,6 @@ public class SpringSchedulingTest {
 	
 	@Test public void annotationSpringScheduling() throws Exception {		
 		Assert.assertTrue("job should have been launched several times already",annotationCountDownLatch.await(10, TimeUnit.SECONDS));
-	}
-		
-	public static class CountDownTasklet implements Tasklet {
-
-		private CountDownLatch countDownLatch;
-		
-		@Override
-		public RepeatStatus execute(StepContribution contribution,
-				ChunkContext chunkContext) throws Exception {
-			countDownLatch.countDown();
-			return RepeatStatus.FINISHED;
-		}
-		
-		public void setCountDownLatch(CountDownLatch countDownLatch) {
-			this.countDownLatch = countDownLatch;
-		}
 	}
 	
 }
