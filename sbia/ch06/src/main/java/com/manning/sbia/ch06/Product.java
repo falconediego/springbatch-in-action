1aaeaa2c3c0407454cc1a3e1bbb2c52983cb9ae1
templth <templth@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2010-10-04 12:50:16 +0000

    Updated content

--- sbia/ch06/src/main/java/com/manning/sbia/ch06/Product.java
+++ sbia/ch06/src/main/java/com/manning/sbia/ch06/Product.java
@@ -3,14 +3,23 @@
  */
 package com.manning.sbia.ch06;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * @author templth
  *
  */
+@Entity
 public class Product {
+	@Id
 	private String id;
+	@Column
 	private String name;
+	@Column
 	private String description;
+	@Column
 	private float price;
 
 	public String getId() { return id; }
--- sbia/ch06/src/main/java/com/manning/sbia/ch06/service/ProductServiceImpl.java
+++ sbia/ch06/src/main/java/com/manning/sbia/ch06/service/ProductServiceImpl.java
@@ -1,6 +1,11 @@
 package com.manning.sbia.ch06.service;
 
-public class ProductServiceImpl {
+import java.util.List;
+import java.util.ArrayList;
+
+import com.manning.sbia.ch06.Product;
+
+public class ProductServiceImpl implements ProductService {
 
 	private Product createProduct(String id, String name, String description, float price) {
 		Product product = new Product();
@@ -13,14 +18,14 @@ public class ProductServiceImpl {
 
 	public List<Product> getProducts() {
 		List<Product> products = new ArrayList<Product>();
-		products.add(createProduct("PR....210", "BlackBerry 8100 Pearl", "", 124.60));
-		products.add(createProduct("PR....211", "Sony Ericsson W810i", "", 139.45));
-		products.add(createProduct("PR....212", "Samsung MM-A900M Ace", "", 97.80));
-		products.add(createProduct("PR....213", "Toshiba M285-E 14", "", 166.20));
-		products.add(createProduct("PR....214", "Nokia 2610 Phone", "", 145.50));
-		products.add(createProduct("PR....215", "CN Clogs Beach/Garden Clog", "", 190.70));
-		products.add(createProduct("PR....216", "AT&T 8525 PDA", "", 289.20));
-		products.add(createProduct("PR....217", "Canon Digital Rebel XT 8MP Digital SLR Camera", "", 13.70));
+		products.add(createProduct("PR....210", "BlackBerry 8100 Pearl", "", 124.60f));
+		products.add(createProduct("PR....211", "Sony Ericsson W810i", "", 139.45f));
+		products.add(createProduct("PR....212", "Samsung MM-A900M Ace", "", 97.80f));
+		products.add(createProduct("PR....213", "Toshiba M285-E 14", "", 166.20f));
+		products.add(createProduct("PR....214", "Nokia 2610 Phone", "", 145.50f));
+		products.add(createProduct("PR....215", "CN Clogs Beach/Garden Clog", "", 190.70f));
+		products.add(createProduct("PR....216", "AT&T 8525 PDA", "", 289.20f));
+		products.add(createProduct("PR....217", "Canon Digital Rebel XT 8MP Digital SLR Camera", "", 13.70f));
 		return products;
 	}
 }
