9ab9c9b88c36bd69dd8a74895d86b2564ed87855
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2010-11-16 22:21:25 +0000

    sandbox: removed robustness samples. Started an XA sample.

---
+++ sandbox/src/test/java/com/manning/sbia/sandbox/xa/XaTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.manning.sbia.sandbox.xa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class XaTest {
+
+	@Test public void xa() {
+		
+	}
+	
+}
