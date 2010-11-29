94c03ca5e0d5acb9e293fdf4baf4044e694416a5
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2010-11-29 16:48:07 +0000

    ch. 09: more retry samples + transaction reader samples.

---
+++ trunk/sbia/ch09/src/test/java/com/manning/sbia/ch09/transaction/JmsItemReader.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.manning.sbia.ch09.transaction;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.jms.core.JmsTemplate;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+public class JmsItemReader<T> implements ItemReader<T> {
+	
+	private JmsTemplate jmsTemplate;
+	
+	private static final Logger LOG = LoggerFactory.getLogger(JmsItemReader.class);
+
+	@Override
+	public T read() throws Exception, UnexpectedInputException, ParseException,
+			NonTransientResourceException {
+		Object object = jmsTemplate.receiveAndConvert();
+		LOG.debug("read "+object);
+		return (T) object;
+	}
+	
+	public void setJmsTemplate(JmsTemplate jmsTemplate) {
+		this.jmsTemplate = jmsTemplate;
+	}
+	
+}
