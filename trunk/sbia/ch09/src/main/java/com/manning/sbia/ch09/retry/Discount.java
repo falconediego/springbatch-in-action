0904be2d436d91d1420e7887970ef458c660c3e2
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2010-12-02 16:24:35 +0000

    ch. 09: added sample for programmatic/transparent retry in application code.

---
+++ trunk/sbia/ch09/src/main/java/com/manning/sbia/ch09/retry/Discount.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.manning.sbia.ch09.retry;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+public class Discount {
+
+}
---
+++ trunk/sbia/ch09/src/main/java/com/manning/sbia/ch09/retry/DiscountService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.manning.sbia.ch09.retry;
+
+import java.util.List;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+public interface DiscountService {
+
+	List<Discount> getDiscounts();
+	
+}
