b79a944486a2accc53721fb82be5092abdc57c0a
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2010-12-06 10:58:16 +0000

    ch. 09: overide equals and hashCode for the items' classes in the best effort sample.

--- sbia/ch09/src/main/java/com/manning/sbia/ch09/domain/Order.java
+++ sbia/ch09/src/main/java/com/manning/sbia/ch09/domain/Order.java
@@ -30,5 +30,32 @@ public class Order implements Serializable {
 	public List<OrderItem> getItems() {
 		return items;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((orderId == null) ? 0 : orderId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (orderId == null) {
+			if (other.orderId != null)
+				return false;
+		} else if (!orderId.equals(other.orderId))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
--- sbia/ch09/src/main/java/com/manning/sbia/ch09/domain/OrderItem.java
+++ sbia/ch09/src/main/java/com/manning/sbia/ch09/domain/OrderItem.java
@@ -28,5 +28,33 @@ public class OrderItem implements Serializable {
 	public short getQuantity() {
 		return quantity;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((productId == null) ? 0 : productId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (productId == null) {
+			if (other.productId != null)
+				return false;
+		} else if (!productId.equals(other.productId))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
