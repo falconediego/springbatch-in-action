7d99c8a9ae9e18e07643302b54bed40104511e4d
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2010-12-15 01:07:53 +0000

    split ch. 09 code into new ch. 10.

---
+++ trunk/sbia/ch10/src/test/java/com/manning/sbia/ch10/BusinessService.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.manning.sbia.ch10;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+public interface BusinessService {
+	
+	String reading();
+
+	void writing(String item);
+	
+	void processing(String item);
+	
+}
