86e32adcbd0206bbe5df7e8ace3ff45dbab6a235
templth <templth@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2010-12-22 11:08:07 +0000

    Added content for chapter 13

---
+++ sbia/ch13/src/tmp/main/java/com/manning/sbia/ch13/LaunchMonitoring.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.manning.sbia.ch13;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+/**
+ * Launching the monitoring.
+ * @author templth
+ *
+ */
+public class LaunchMonitoring {
+
+	private static Server startH2Server() throws SQLException {
+		// start the TCP Server
+		Server server = Server.createTcpServer(new String[0]).start();
+		
+		return server;
+	}
+	
+	private static void stopH2Server(Server server) {
+		// stop the TCP Server
+		server.stop();
+	}
+	
+	public static void main(String[] args) {
+		Server server = null;
+		ClassPathXmlApplicationContext context = null;
+		try {
+			server = startH2Server();
+			context = new ClassPathXmlApplicationContext(new String[] {
+					"/com/manning/sbia/ch13/root-database-context.xml",
+					"/com/manning/sbia/ch13/batch-infrastructure-context.xml",
+					"/com/manning/sbia/ch13/batch-monitoring-context.xml"
+			});
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			while (true) {
+				String input = reader.readLine();
+				if( "exit".equals(input)) {
+					break;
+				}
+			}
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (server!=null) {
+				stopH2Server(server);
+			}
+		}
+	}
+	
+}
