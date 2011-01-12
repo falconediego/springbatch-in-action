6730d734ca50a5aade85e8ddb51d3e90b544fa98
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2011-01-12 16:48:40 +0000

    ch. 03: upgraded to spring batch admin 1.2.0. No more WAR archive in the demo, just used the maven dependencies.

--- trunk/sbia/ch03/src/main/java/com/manning/sbia/ch03/LaunchSpringBatchAdmin.java
+++ trunk/sbia/ch03/src/main/java/com/manning/sbia/ch03/LaunchSpringBatchAdmin.java
@@ -18,22 +18,19 @@ public class LaunchSpringBatchAdmin {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		System.setProperty("ENVIRONMENT", "h2");
-		System.setProperty("batch.data.source.init", "false");
 		Server server = new Server();
-        Connector connector = new SelectChannelConnector();
-        connector.setPort(8080);
-        connector.setHost("127.0.0.1");
-        server.addConnector(connector);
-              
+		Connector connector = new SelectChannelConnector();
+		connector.setPort(8080);
+		connector.setHost("127.0.0.1");
+		server.addConnector(connector);
 
-        WebAppContext wac = new WebAppContext();
-        wac.setContextPath("/springbatchadmin");
-        wac.setWar("./spring-batch-admin-sample-1.0.0.RELEASE.war");
-        server.setHandler(wac);
-        server.setStopAtShutdown(true);
-
-        server.start();
+		WebAppContext wac = new WebAppContext();
+		wac.setContextPath("/springbatchadmin");
+		wac.setWar("./src/main/webapp");
+		server.setHandler(wac);
+		server.setStopAtShutdown(true);
+		
+		server.start();
         
         System.out.println("**** Spring Batch Admin launched");
 
