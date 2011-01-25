1f9b498ad2723731a240508065cbaad663575a45
olivier.bazoud@gmail.com <olivier.bazoud@gmail.com@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2011-01-25 18:48:32 +0000

    ch 07 : add mail writer

---
+++ trunk/sbia/ch07/src/main/java/com/manning/sbia/ch07/mail/CustomerItemProcessor.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2010 FullSIX
+ *
+ * $Id$
+ */
+package com.manning.sbia.ch07.mail;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.mail.SimpleMailMessage;
+
+/**
+ * @author bazoud
+ */
+public class CustomerItemProcessor implements ItemProcessor<Customer, SimpleMailMessage> {
+  @Override
+  public SimpleMailMessage process(Customer item) throws Exception {
+    SimpleMailMessage msg = new SimpleMailMessage();
+    msg.setFrom("springbatchinaction@test.com");
+    msg.setTo(item.getEmail());
+    msg.setSubject("Welcome message !!");
+    msg.setText("Hello " + item.getName());
+    return msg;
+  }
+}
