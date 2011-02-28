4f3af9c302e212a46a143f57a0ace1649280febd
templth <templth@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2011-02-28 08:57:08 +0000

    Fixed problems

--- sbia/ch13/src/test/java/com/manning/sbia/ch13/JobStructureTest.java
+++ sbia/ch13/src/test/java/com/manning/sbia/ch13/JobStructureTest.java
@@ -5,8 +5,11 @@ package com.manning.sbia.ch13;
 
 import java.util.Date;
 
+import javax.sql.DataSource;
+
 import org.junit.Test;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
 /**
@@ -18,6 +21,9 @@ import org.springframework.test.context.ContextConfiguration;
 	"classpath:/com/manning/sbia/ch13/JobStructureTest-context.xml"})
 public class JobStructureTest extends AbstractJobStructureTest {
 
+	@Autowired
+	private DataSource dataSource;
+	
 	@Test public void delimitedJob() throws Exception {
 		JobParametersBuilder parametersBuilder = new JobParametersBuilder();
 		parametersBuilder.addDate("date", new Date());
