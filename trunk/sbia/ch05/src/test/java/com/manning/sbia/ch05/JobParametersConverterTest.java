341ef0e481edbc776d852785e923042698b25730
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2011-03-12 18:21:40 +0000

    ch. 05: corrected job converter test for make it portable accross OS.

--- trunk/sbia/ch05/src/test/java/com/manning/sbia/ch05/JobParametersConverterTest.java
+++ trunk/sbia/ch05/src/test/java/com/manning/sbia/ch05/JobParametersConverterTest.java
@@ -3,6 +3,8 @@
  */
 package com.manning.sbia.ch05;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Properties;
 
@@ -19,18 +21,23 @@ import org.springframework.util.StringUtils;
  */
 public class JobParametersConverterTest {
 
-	@Test public void convert() {
+	@Test public void convert() throws Exception {
+		// trick to make the test portable (for double) on different OS
+		// the default number format used by the converter truncates digits
+		double doubleParam = 23.4;
+		NumberFormat format = new DecimalFormat("#"); // the one used in the converter
+		doubleParam = format.parse(format.format(doubleParam)).doubleValue();
 		Properties params = StringUtils.splitArrayElementsIntoProperties(new String[]{
 			"someString(string)=someStringValue",
 			"someDate(date)=2010/12/08",
 			"someLong(long)=23",
-			"someDouble(double)=23.4"
+			"someDouble(double)="+format.format(doubleParam)
 		}, "=");
 		JobParametersConverter converter = new DefaultJobParametersConverter();
 		JobParameters parameters = converter.getJobParameters(params);
 		Assert.assertEquals("someStringValue",parameters.getString("someString"));
 		Assert.assertEquals(23L,parameters.getLong("someLong"));
-		Assert.assertEquals(23.4,parameters.getDouble("someDouble"),0);
+		Assert.assertEquals(doubleParam,parameters.getDouble("someDouble"),0);
 		Assert.assertEquals(
 			"2010/12/08",
 			new SimpleDateFormat("yyyy/MM/dd").format(parameters.getDate("someDate"))
