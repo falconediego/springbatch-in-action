65db7412d8199563065fdffc4b7d300a46ae0735
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2011-04-12 13:12:58 +0000

    ch. 01: removed last residues from chapters 1 & 2 merging.

--- trunk/sbia/ch01/src/test/java/com/manning/sbia/ch01/batch/ImportProductsIntegrationTest.java
+++ trunk/sbia/ch01/src/test/java/com/manning/sbia/ch01/batch/ImportProductsIntegrationTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -68,32 +67,6 @@ public class ImportProductsIntegrationTest {
 		);
 		int nbOfNewProducts = 6;
 		Assert.assertEquals(initial+nbOfNewProducts,jdbcTemplate.queryForInt("select count(1) from product"));
-	}
-	
-	@Test public void missingParameters() throws Exception {		
-		try {
-			jobLauncher.run(job, new JobParametersBuilder()
-				.addString("inputResource", "classpath:/input/products_with_errors.zip")
-				.toJobParameters()
-			);
-			Assert.fail("missing parameters, the job should not have been launched");
-		} catch (JobParametersInvalidException e) {
-			// OK
-		}		
-	}
-	
-	@Test public void inputDoesNotExist() throws Exception {	
-		try {
-			jobLauncher.run(job, new JobParametersBuilder()
-				.addString("inputResource", "classpath:/input/bad_products_input.zip")
-				.addString("targetDirectory", "./target/importproductsbatch/")
-				.addString("targetFile","products.txt")
-				.toJobParameters()
-			);
-			Assert.fail("the input does not exist, the job should not have been launched");
-		} catch (JobParametersInvalidException e) {
-			// OK	
-		}		
 	}
 	
 }
