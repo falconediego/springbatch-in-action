9570c5a9087e38657415171491fd0553ea2dfcbe
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2011-04-13 11:15:21 +0000

    removed residues from former chapter 13.

--- sbia/ch13/src/main/java/com/manning/sbia/ch13/batch/BatchMonitoringNotifier.java
+++
@@ -1,7 +0,0 @@
-package com.manning.sbia.ch13.batch;
-
-import org.springframework.batch.core.JobExecution;
-
-public interface BatchMonitoringNotifier {
-	void notify(JobExecution jobExecution);
-}
