87159c96902d43eceac51c0ab6c736d4e4963a59
acogoluegnes <acogoluegnes@23977762-bf59-b1c5-5b28-d4b9f2eccbaa>
2011-04-13 11:40:28 +0000

    removed residues from former chapter 14.

--- sbia/ch14/src/test/resources/create-tables.sql
+++
@@ -1,8 +0,0 @@
-create table product (
-	id varchar(255) not null,
-	name varchar(255) not null,
-	description varchar(255) not null,
-	price float not null,
-	processed boolean not null default false,
-	primary key (id)
-);
